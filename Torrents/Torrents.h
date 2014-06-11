335e4d29a4dd826ba9a362b0b55c15a643330021
Alexandros Salazar <nomothetis@gmail.com>
2014-06-10 20:53:00 -0400

    Initial Commit

---
+++ Torrents/Torrents.h
@@ -0,0 +1,19 @@
+//
+//  Torrents.h
+//  Torrents
+//
+//  Created by Salazar, Alexandros on 6/10/14.
+//  Copyright (c) 2014 nomothetis. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Torrents.
+FOUNDATION_EXPORT double TorrentsVersionNumber;
+
+//! Project version string for Torrents.
+FOUNDATION_EXPORT const unsigned char TorrentsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Torrents/PublicHeader.h>
+
+
