9fa4a3603f90df4b335e25c731e5dd7d80e40a3e
Alexandros Salazar <nomothetis@gmail.com>
2014-06-11 22:08:13 -0400

    A better iteration; still not compiling.

--- Torrents/Stream.swift
+++ Torrents/Stream.swift
@@ -11,19 +11,43 @@ import Foundation
 /*
  A stream generator generates the items of a sequence on demand.
  */
-protocol Gen {
-    typealias ItemType
-    func next() -> ItemType
+protocol Generator {
+    typealias GeneratorType
+    func next() -> GeneratorType
+}
+
+/*
+ A map generator is a class that represents mapping over a generator.
+ */
+class MapGenerator<P, G where G: Generator>:Generator {
+    typealias ItemType = P
+    let baseGenerator:G
+    let mapFunction:G.GeneratorType -> P
+    
+    init(baseGenerator:G, f:G.GeneratorType -> P) {
+        self.baseGenerator = baseGenerator
+        self.mapFunction = f
+    }
+    
+    func next() -> P {
+        return self.mapFunction(self.baseGenerator.next())
+    }
 }
 
 /*
 A stream is a data structure that lazily computes its elements on demand. Traversal methods are
 defined that allow .
 */
-class Stream<T, G where G: Gen> {
-    var generator:G
-    var memoizedData:Array<T> = []
-    init(theGenerator:G, memo:Array<T> = []) {
+class Stream<T, G where G: Generator> {
+    let generator:G
+    var memoizedData:Array<G.GeneratorType>
+    init(theGenerator:G, memo:Array<G.GeneratorType> = []) {
         self.generator = theGenerator
+        self.memoizedData = memo
+    }
+    
+    func map<P>(f:G.GeneratorType -> P) -> Stream<P, G> {
+        var memoizedMap = memoizedData.map(f)
+        return Stream(MapGenerator(self.generator, f), memoizedMap)
     }
 }
